9a96fe3233369a16fa122cd425850dfd7312ba15
Igor Carvalho <igor822@gmail.com>
2014-02-02 17:46:40 -0200

    Implemented method to close global modals

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,15 +1,27 @@
 (function($){
- 
-    $.fn.extend({ 
-         
+
+    $.leanModal = function() {
+
+    };
+
+    $.leanModal.close = function() {
+        $("#lean_overlay").fadeOut(200);
+        $('.modal_panel').css({ 'display' : 'none' });
+    };
+
+    $.fn.extend({
+
         leanModal: function(options) {
  
             var defaults = {
                 top: 100,
                 overlay: 0.5,
                 closeButton: null,
-                onClose : null
-            }
+                onClose : null,
+                onOpen : null,
+                onInit : null,
+                allow_close : true
+            };
             
             var overlay = $("<div id='lean_overlay'></div>");
             
@@ -20,55 +32,62 @@
             return this.each(function() {
             
                 var o = options;
-               
-                $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
-
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id, o.onClose);                    
-                });
                 
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id, o.onClose);                    
+                $(this).click(function(e) {
+                    if (typeof o.onOpen == 'function') {
+                        o.onOpen(e, o, $(this));
+                    }
+                    open_modal(e, o, this);
                 });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
-
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
-
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
-
-        		$(modal_id).fadeTo(200,1);
-
-                e.preventDefault();
-                		
-              	});
              
             });
+            
+            function open_modal(e, o, _this) {
+                $this = _this;
+                var modal_id = $($this).attr("href");
 
-			function close_modal(modal_id, obj){
-				if (typeof obj == 'function') {
-					obj();
-				}
-        		$("#lean_overlay").fadeOut(200);
+                if (o.allow_close == true) {
+                    $("#lean_overlay").click(function() {
+                         close_modal(modal_id, o.onClose);
+                    });
+                    $(o.closeButton).click(function() {
+                         close_modal(modal_id, o.onClose);
+                    });
+                } else {
+                    $(o.closeButton).hide();
+                }
+                            
+                var modal_height = $(modal_id).outerHeight();
+                var modal_width = $(modal_id).outerWidth();
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
+                $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+                $('#lean_overlay').fadeTo(200,o.overlay);
+
+                $(modal_id).css({
+                    'display' : 'block',
+                    'position' : 'fixed',
+                    'opacity' : 0,
+                    'z-index': 11000,
+                    'left' : 50 + '%',
+                    'margin-left' : -(modal_width/2) + "px",
+                    'top' : o.top + "px"
+                
+                });
+
+                $(modal_id).fadeTo(200,1);
+
+                e.preventDefault();
+            }
+
+            function close_modal(modal_id, obj){
+                if (typeof obj == 'function') {
+                    obj();
+                }
+                $("#lean_overlay").fadeOut(200);
+
+                $(modal_id).css({ 'display' : 'none' });
+            
+            }
     
         }
     });
